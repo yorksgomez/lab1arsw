8a1cf5172a24b6b3ec2cb721dd2405b963094a0f
Héctor Cadavid <hector.cadavid@gmail.com>
2017-08-09 11:04:47 -0500

    first commit

--- src/main/java/edu/eci/arsw/math/PiDigits.java
+++ src/main/java/edu/eci/arsw/math/PiDigits.java
@@ -4,18 +4,20 @@ package edu.eci.arsw.math;
 ///  An implementation of the Bailey-Borwein-Plouffe formula for calculating hexadecimal
 ///  digits of pi.
 ///  https://en.wikipedia.org/wiki/Bailey%E2%80%93Borwein%E2%80%93Plouffe_formula
+///  *** Translated from C# code: https://github.com/mmoroney/DigitsOfPi ***
 ///  </summary>
 public class PiDigits {
 
     private static int DigitsPerSum = 8;
     private static double Epsilon = 1e-17;
 
-    /// <summary>
-    /// Returns a range of hexadecimal digits of pi.
-    /// </summary>
-    /// <param name="start">The starting location of the range.</param>
-    /// <param name="count">The number of digits to return.</param>
-    /// <returns>An array containing the hexadecimal digits.</returns>
+    
+    /**
+     * Returns a range of hexadecimal digits of pi.
+     * @param start The starting location of the range.
+     * @param count The number of digits to return
+     * @return An array containing the hexadecimal digits.
+     */
     public static byte[] getDigits(int start, int count) {
         if (start < 0) {
             throw new RuntimeException("Invalid Interval");
